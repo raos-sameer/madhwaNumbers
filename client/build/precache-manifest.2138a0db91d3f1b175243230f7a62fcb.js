51e59ddb6b4b429476dbb8e5fd78c2f40b632217
Sameer Rao <rao.s.sameer@gmail.com>
2020-07-04 15:03:42 +0530

    HariSarvottama

---
+++ client/build/precache-manifest.2138a0db91d3f1b175243230f7a62fcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d85bc2da29ffaaa8b163913af9f739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce8fa46bf7ce7efebb7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e80aa9f4b18dd0ee1f4d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dce8fa46bf7ce7efebb7",
+    "url": "/static/js/2.7f1f6a55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7f1f6a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80aa9f4b18dd0ee1f4d",
+    "url": "/static/js/main.6bac7270.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
