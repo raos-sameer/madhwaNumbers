c5fb54fc7d39c3f42eb0b280ca10171156aefe66
Sameer Rao <rao.s.sameer@gmail.com>
2020-07-04 14:12:27 +0530

    Revisit

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use("/api/", routes);
 app.use("/faq/", faqRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client\build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client\build", index.html)); // <- try "index.html"
+    res.sendFile(path.resolve(__dirname, "client/build", index.html)); // <- try "index.html"
   });
 }
 app.listen(PORT, console.log(`Server running on ${PORT}`));
